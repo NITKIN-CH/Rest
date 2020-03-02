14243a8004dcad60ea4cc2f96d23a43b68119629
NITKIN-CH <52566505+NITKIN-CH@users.noreply.github.com>
2020-03-02 17:24:05 +0300

    1

---
+++ src/main/java/Controller/ExampleRestController.java
@@ -0,0 +1,42 @@
+package Controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExampleRestController {
+    public static class  RestResponse{
+        private String param1;
+        private String param2;
+
+        public String getParam1() {
+            return param1;
+        }
+
+        public void setParam1(String param1) {
+            this.param1 = param1;
+        }
+
+        public String getParam2() {
+            return param2;
+        }
+
+        public void setParam2(String param2) {
+            this.param2 = param2;
+        }
+    }
+
+    @RequestMapping(value = "/misha", method = RequestMethod.GET,
+    produces = MediaType.APPLICATION_JSON_VALUE)
+    public RestResponse restMethod(String name) {
+        RestResponse result = new RestResponse();
+        result.setParam1("Misha");
+        result.setParam2(name);
+
+        return result;
+    }
+
+}
+
